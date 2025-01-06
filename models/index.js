ee0da9f4b2f340cf91867acc3f952ea0a066a6ef
Phresh . <marquisej.galo@gmail.com>
2025-01-06 17:42:35 -0500

    COMPLETEING MODELS

--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category (linking child and parent through foreign key)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete:"CASCADE"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  }
+})
 
 module.exports = {
   Product,
