c414b11542a7bc0dc7526e5511e986602addd806
Phresh . <marquisej.galo@gmail.com>
2025-01-07 18:40:47 -0500

    adding api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,17 +2,34 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
+//http://localhost:3001/api/products/
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const categoryData = await Product.findAll({
+     include: [{ model: Category }, { model: Tag, through: ProductTag }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const categoryData = await Product.findByPk(
+      req.params.id,{
+     include: [{ model: Category }, { model: Tag, through: ProductTag }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -92,8 +109,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const categoryData = await Product.findAll({
+     where:{
+      id:req.params.id
+     }
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
